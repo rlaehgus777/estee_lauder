cdf675bfb1b4ec4362dddc508b929af6c56b73c0
rlaehgus777 <joker112960@gmail.com>
2026-09-03 16:54:48 +0900

    0903

---
+++ script/esteedb.js
@@ -0,0 +1,39 @@
+//esteeDB.js
+
+const bestProduct = [{
+    id: 1,
+    image : './images/thumnail1.jpg',
+    name : '다이아몬드 에이지 리버셜 리커버리크림',
+    price :'KRW 575,000',
+},
+{
+    id: 2,
+    image : './images/thumnail2.jpg',
+    name : '어드벤스드 나이트 리페어 갈색병세럼',
+    price :'KRW 34,000~',
+},
+{
+    id: 3,
+    image : './images/thumnail3.jpg',
+    name : '리바이탈라이징 수프림 플러스 유스 파워소프트 크림',
+    price : 'KRW 118,000',
+},
+{
+    id: 4,
+    image : './images/thumnail4.jpg',
+    name : '수프림 플러스 나이트 파워 바운스 크림',
+    price : 'KRW 120,000~',
+},
+{
+    id: 5,
+    image : './images/thumnail5.jpg',
+    name : '마이크로 에센스 트리트먼트 로션',
+    price : 'KRW 105,000~',
+},
+{
+    id: 6,
+    image : './images/thumnail6.jpgs',
+    name : '얼티미트 다이아몬드 리버설 리커버리 크림',
+    price : 'KRW 575,000',
+}
+]
