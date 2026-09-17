3fb76922d31588af88710da248afd36a0330c23d
rlaehgus777 <joker112960@gmail.com>
2026-09-17 15:59:10 +0900

    0917

--- script/product_page.js
+++ script/product_page.js
@@ -182,17 +182,179 @@ const color3w1 = document.querySelector('.middle > .color_chip li:nth-child(8)')
 const colorPicker = document.querySelector('#color_select_form .middle .color_picker img');
 const colorResult = document.querySelector('#color_select_form > .middle .option_info');
 
+color1c0.addEventListener('click',()=>{
+    colorPicker.style.transform = "translateX(8px)";
+    colorResult.textContent = '1C0 쉘';
+})
 color1w1.addEventListener('click',()=>{
     colorPicker.style.transform = "translateX(53px)";
     colorResult.textContent = '1W1 본';
+})
 
+color1w2.addEventListener('click',()=>{
+    colorPicker.style.transform = "translateX(96px)";
+    colorResult.textContent = '1W2 샌드';
+})
+color1n0.addEventListener('click',()=>{
+    colorPicker.style.transform = "translateX(142px)";
+    colorResult.textContent = '1N0 포슬린';
+})
+color1n1.addEventListener('click',()=>{
+    colorPicker.style.transform = "translateX(185px)";
+    colorResult.textContent = '1N1 아이보리 누드';
+})
+color2c0.addEventListener('click',()=>{
+    colorPicker.style.transform = "translateX(230px)";
+    colorResult.textContent = '2C0 쿨바닐라';
+})
+color2w0.addEventListener('click',()=>{
+    colorPicker.style.transform = "translateX(274px)";
+    colorResult.textContent = '2W0 웜바닐라';
+})
+color3w1.addEventListener('click',()=>{
+    colorPicker.style.transform = "translateX(318px)";
+    colorResult.textContent = '3W1 토니';
 })
 
 
 
-/* ================옵션 선택 */
+/* ================옵션 선택시 펼쳐지기 */
 const colorSelectBtn = document.querySelector('#color_select_btn');
 const colorList = document.querySelector('.select_list');
+const colorSelectArrow = document.querySelector('#color_select_btn .more img')
 colorSelectBtn.addEventListener('click',()=>{
     colorList.classList.toggle('active');
+    colorSelectArrow.classList.toggle('active');
 })
+
+
+
+/* ======================옵션 선택 시 반영되기 */
+const optionChecker = document.querySelectorAll('#color_select_btn span');
+const option1c0 = document.querySelector('.select_list > li:nth-child(1)');
+const option1w1 = document.querySelector('.select_list > li:nth-child(2)');
+const option1w2 = document.querySelector('.select_list > li:nth-child(3)');
+const option1n0 = document.querySelector('.select_list > li:nth-child(4)');
+const option1n1 = document.querySelector('.select_list > li:nth-child(5)');
+const option2c0 = document.querySelector('.select_list > li:nth-child(6)');
+const option2w0 = document.querySelector('.select_list > li:nth-child(7)');
+const option3w1 = document.querySelector('.select_list > li:nth-child(8)');
+
+const optionResult = document.querySelector('.option_result');
+const optionResultName = document.querySelector('.option_result .top .option_name');
+const optionResultClose = document.querySelector('#option_result_close');
+
+optionResultClose.addEventListener('click',()=>{
+    optionResult.style.display = 'none';
+})
+console.log(optionChecker);
+
+option1c0.addEventListener('click',()=>{
+    optionChecker[0].style.backgroundColor = "#DAB3A2";
+    optionChecker[1].textContent = "1C0 쉘";
+    colorList.classList.toggle('active');
+    colorSelectArrow.classList.toggle('active');
+    optionResult.style.display = "block";
+    optionResultName.textContent = "1C0 쉘";
+})
+option1w1.addEventListener('click',()=>{
+    optionChecker[0].style.backgroundColor = "#CBA986";
+    optionChecker[1].textContent = "1W1 본";
+    colorList.classList.toggle('active');
+    colorSelectArrow.classList.toggle('active');
+    optionResult.style.display = "block";
+    optionResultName.textContent = "1W1 본";
+})
+option1w2.addEventListener('click',()=>{
+    optionChecker[0].style.backgroundColor = "#CAA27F";
+    optionChecker[1].textContent = "1W2 샌드";
+    colorList.classList.toggle('active');
+    colorSelectArrow.classList.toggle('active');
+    optionResult.style.display = "block";
+    optionResultName.textContent = "1W2 샌드";
+})
+option1n0.addEventListener('click',()=>{
+    optionChecker[0].style.backgroundColor = "#CBA98E";
+    optionChecker[1].textContent = "1N0 포슬린";
+    colorList.classList.toggle('active');
+    colorSelectArrow.classList.toggle('active');
+    optionResult.style.display = "block";
+    optionResultName.textContent = "1N0 포슬린";
+})
+option1n1.addEventListener('click',()=>{
+    optionChecker[0].style.backgroundColor = "#FFC8A3";
+    optionChecker[1].textContent = "1N1 아이보리 누드";
+    colorList.classList.toggle('active');
+    colorSelectArrow.classList.toggle('active');
+    optionResult.style.display = "block";
+    optionResultName.textContent = "1N1 아이보리 누드";
+})
+option2c0.addEventListener('click',()=>{
+    optionChecker[0].style.backgroundColor = "#CBA284";
+    optionChecker[1].textContent = "2C0 쿨바닐라";
+    colorList.classList.toggle('active');
+    colorSelectArrow.classList.toggle('active');
+    optionResult.style.display = "block";
+    optionResultName.textContent = "2C0 쿨바닐라";
+})
+option2w0.addEventListener('click',()=>{
+    optionChecker[0].style.backgroundColor = "#C19D7D";
+    optionChecker[1].textContent = "2W0 웜바닐라";
+    colorList.classList.toggle('active');
+    colorSelectArrow.classList.toggle('active');
+    optionResult.style.display = "block";
+    optionResultName.textContent = "2W0 웜바닐라";
+})
+option3w1.addEventListener('click',()=>{
+    optionChecker[0].style.backgroundColor = "#C0966E";
+    optionChecker[1].textContent = "3W1 토니";
+    colorList.classList.toggle('active');
+    colorSelectArrow.classList.toggle('active');
+    optionResult.style.display = "block";
+    optionResultName.textContent = "3W1 토니";
+})
+
+
+
+/* ================================수량 조절 */
+const optionMinus = document.querySelector('#option_minus');
+const optionMinusImg = document.querySelector('#option_minus > img');
+const optionPlus = document.querySelector('#option_plus');
+const optionNumber = document.querySelector('#option_number');
+let optionChangeNum = Number(optionNumber.textContent);
+const finalPrice = document.querySelector('.final_price');
+
+const price = 90000;
+
+console.log(typeof optionChangeNum);
+
+function priceUpdate() {
+    finalPrice.textContent = 'KRW ' + (price * optionChangeNum).toLocaleString();
+}
+
+optionPlus.addEventListener('click',()=>{
+    optionChangeNum++;
+    optionNumber.textContent = optionChangeNum;
+    if(optionChangeNum >= 1){
+        optionMinusImg.src = "./images/minus_active_icon.svg";
+        iconUpdate();
+        priceUpdate()
+    }
+})
+
+optionMinus.addEventListener('click',()=>{
+    if(optionChangeNum > 1){
+        optionChangeNum--;
+        optionNumber.textContent = optionChangeNum;
+        iconUpdate();
+        priceUpdate()
+    }
+})
+
+function iconUpdate(){
+    if (optionChangeNum > 1) {
+        optionMinusImg.src = "./images/minus_active_icon.svg";
+    } else {
+        optionMinusImg.src = "./images/option_minus.svg";
+    }
+}
